88f97c877c9748838ebf3b9c24a7fd45de914b68
bikash4jp <bikash4jp@gmail.com>
2025-12-09 15:22:11 +0900

    Stop tracking node_modules and build artifacts

--- dist/routes/transactions.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/routes/transactions.routes.ts
-const express_1 = require("express");
-const entries_controller_1 = require("../controllers/entries.controller");
-const router = (0, express_1.Router)();
-// GET /transactions
-router.get('/', entries_controller_1.listTransactions);
-exports.default = router;
